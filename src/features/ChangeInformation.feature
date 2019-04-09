3096533266b9740516abc253d6af69c291dc74b5
Simon Moe Sørensen <moe.simon@gmail.com>
2019-04-09 21:23:43 +0200

    Scenario: A clerk changes person information Working.

--- src/features/ChangeInformation.feature
+++ src/features/ChangeInformation.feature
@@ -6,3 +6,11 @@ Feature: ChangeInformation
     And a person with information
     When he gets new PI about a person
     Then he should change the information
+
+  Scenario: A clerk changes person information
+    Given a clerk
+    And a person with information
+    When the information is wrong
+    Then the clerk should be able to change this information
+
+
--- src/test/java/stepdefs/ChangeInformationSteps.java
+++ src/test/java/stepdefs/ChangeInformationSteps.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import management.Clerk;
 import management.ICTOfficer;
 import management.Patient;
 
@@ -11,11 +12,11 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class ChangeInformationSteps {
     private ICTOfficer ictOfficer = new ICTOfficer();
+    private Clerk clerk = new Clerk();
     private Patient patientWithInformation = new Patient();
     private Patient patientWithoutInformation = new Patient();
 
@@ -46,7 +47,7 @@ public class ChangeInformationSteps {
 
     @When("he gets new PI about a person")
     public void heGetsNewPIAboutAPerson() {
-        // Patient is born in year 1990 and address needs to be changed..
+        // Patient changed phone number
 
         // can't write code for this
     }
@@ -54,17 +55,41 @@ public class ChangeInformationSteps {
     @Then("he should change the information")
     public void heShouldChangeTheInformation() {
         // Get old values
+        int oldPhoneNumber = patientWithInformation.getPhoneNumber();
+
+        // Set new values
+        ictOfficer.setPersonPhoneNumber(patientWithInformation,
+                13371337);
+
+        // Check that the value has changed
+        assertNotEquals(oldPhoneNumber, patientWithInformation.getPhoneNumber());
+        assertEquals(13371337, patientWithInformation.getPhoneNumber());
+    }
+
+    @When("the information is wrong")
+    public void theInformationIsWrong() {
+        // Patient is born in year 1990 and address needs to be changed..
+        // can't write code
+    }
+
+    @Then("the clerk should be able to change this information")
+    public void theClerkShouldBeAbleToChangeThisInformation() {
+        // Get old values
         Date oldDate = patientWithInformation.getBirthdate();
         String oldAddress = patientWithInformation.getHomeAddress();
 
         // Set new values
-        ictOfficer.setPersonBirthdate(patientWithInformation,
+        clerk.setPersonBirthdate(patientWithInformation,
                 new GregorianCalendar(1990, Calendar.JANUARY, 0).getTime());
-        ictOfficer.setPersonHomeAddress(patientWithInformation,
+        clerk.setPersonHomeAddress(patientWithInformation,
                 "Vejen på gaden 10");
 
         // Check that the values have changed
         assertNotEquals(oldAddress, patientWithInformation.getHomeAddress());
         assertNotEquals(oldDate, patientWithInformation.getBirthdate());
+        assertEquals("Vejen på gaden 10", patientWithInformation.getHomeAddress());
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(patientWithInformation.getBirthdate());
+        assertEquals(1990, cal.get(Calendar.YEAR));
     }
 }
