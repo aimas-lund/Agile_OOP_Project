f7423a5c63e4439b692c7ec2e32a38fcd02b4055
simonmoesorensen <moe.simon@gmail.com>
2019-04-24 10:14:28 +0200

    Added Scenario 'a user adds a patient to the database'

--- src/features/Database.feature
+++ src/features/Database.feature
@@ -1,2 +1,7 @@
 Feature: Database
   Handles everything related to the database, not querying.
+
+  Scenario: a user adds a patient to the database
+  Given a user
+  When a new patient is admitted to the hospital
+  Then the user should add the patient to the database
--- src/test/java/stepdefs/DatabaseSteps.java
+++ src/test/java/stepdefs/DatabaseSteps.java
@@ -11,6 +11,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseSteps {
 
+    @Given("a user")
+    public void a_user() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("a new patient is admitted to the hospital")
+    public void a_new_patient_is_admitted_to_the_hospital() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the user should add the patient to the database")
+    public void the_user_should_add_the_patient_to_the_database() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 //
 //    @Given("a hospital")
 //    public void aHospital() {
