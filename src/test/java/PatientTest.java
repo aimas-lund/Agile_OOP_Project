b21ce5e673b00a03a85bab569c6e992524431ba0
AimasLund <aimas.lund@gmail.com>
2019-04-29 23:00:33 +0200

    Added tests to inherited getPersonInformation method in Patient class

--- src/test/java/PatientTest.java
+++ src/test/java/PatientTest.java
@@ -1,13 +1,48 @@
 import management.Patient;
+import java.util.Date;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertSame;
 import static junit.framework.TestCase.assertTrue;
 
 public class PatientTest {
     Patient patient = new Patient();
+    Date birthdate = new Date(1990, 1, 1);
+    Patient patientWithInformation = new Patient(
+            "foo",
+            "bar",
+            birthdate,
+            0,
+            "Foo Bar Street 38",
+            12345678);
 
     @Test
     public void isAlive() {
         assertTrue(patient.isAlive());
     }
+    @Test
+    public void getPersonInformationName() {
+        assertSame("foo", patientWithInformation.getPersonInformation()[1]);
+    }
+    @Test
+    public void getPersonInformationSurname() {
+        assertSame("bar", patientWithInformation.getPersonInformation()[2]);
+    }
+    @Test
+    public void getPersonInformationBirthdate() {
+        assertSame("1990_01_01", patientWithInformation.getPersonInformation()[3]);
+    }
+    @Test
+    public void getPersonInformationGender() {
+        assertSame("0", patientWithInformation.getPersonInformation()[4]);
+    }
+    @Test
+    public void getPersonInformationHomeAddress() {
+        assertSame("Foo Bar Street 38", patientWithInformation.getPersonInformation()[5]);
+    }
+    @Test
+    public void getPersonInformationPhoneNumber() {
+        assertSame("12345678", patientWithInformation.getPersonInformation()[6]);
+    }
+
 }
