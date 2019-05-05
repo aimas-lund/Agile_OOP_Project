f96862d247d2e4c23ce7435d14cf142fe7d70ecf
Mikkel <mikk528a@gmail.com>
2019-05-05 18:47:55 +0200

    Added URL Security layer

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,16 +4,16 @@ function searchPatient() {
     $(".searchPatient").removeAttr("required","false");
     $("#p" + option).show();
     $("#p" + option).attr("required", "true");
-
 }
+
 function searchStaff() {
     var option = $("#searchStaffParam").val();
     $(".searchStaff").hide();
     $(".searchStaff").removeAttr("required","false");
     $("#s" + option).show();
     $("#s" + option).attr("required", "true");
-
 }
+
 $(document).ready(function() {
    $( ".dashboard-menu > h3" ).click(function() {
        $("i",this).toggleClass("fa fa-caret-down fa fa-caret-up")
