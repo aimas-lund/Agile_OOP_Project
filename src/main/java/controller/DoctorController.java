8a62a7b8121cdcf6a3646d95440918d7f357bbaf
Natasha Ørregaard Klingenbrunn <natasha.klingenbrunn@gmail.com>
2019-05-06 20:15:46 +0200

    DoctorController class finished

--- src/main/java/controller/DoctorController.java
+++ src/main/java/controller/DoctorController.java
@@ -8,6 +8,7 @@ import core.persons.Person;
 import exceptions.PersonNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import persistence.data_access_objects.DaoDepartmentImpl;
+import persistence.data_access_objects.DaoPatientImpl;
 
 import javax.print.Doc;
 import java.util.ArrayList;
@@ -16,29 +17,28 @@ import java.util.HashMap;
 @RestController
 public class DoctorController {
 
-    DaoDepartmentImpl<Department> dao = new DaoDepartmentImpl<Department>();
+    DaoDepartmentImpl<Department> daodept = new DaoDepartmentImpl<Department>();
+    DaoPatientImpl<Patient> daopatient = new DaoPatientImpl<Patient>();
 
     Doctor doctor = new Doctor();
     // search patients
 
-//    @PostMapping("/findPatient")
-//    public @ResponseBody
-//    ArrayList<Person> findPatient(
-//            @RequestParam(value = "name", required=false) String name,
-//            @RequestParam(value="id", required =false) String id,
-//            @RequestParam(value = "department", required = false) String department) throws PersonNotFoundException {
-//
-//
-//        HashMap<String, String> hashMap = new HashMap<String, String>();
-//        hashMap.put("name",name);
-//        hashMap.put("id",id);
-//        hashMap.put("department",department);
-//
-//
-//
-//        return QRK.find(hashMap);
-//
-//    }
+    @PostMapping("/searchPatient")
+    public @ResponseBody
+    ArrayList<Patient> findPatient(
+            @RequestParam(value = "name", required=false) String name,
+            @RequestParam(value="id", required =false) String id,
+            @RequestParam(value = "department", required = false) String department) throws PersonNotFoundException {
+
+
+        HashMap<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put("name",name);
+        hashMap.put("id",id);
+        hashMap.put("department",department);
+
+        return daopatient.find(hashMap);
+
+    }
 
     // get waiting patients
 
@@ -47,7 +47,7 @@ public class DoctorController {
     Patient getWaitingPatient(@RequestParam(value = "department") String department){
         HashMap<String, String> hashMap = new HashMap<String, String>();
         hashMap.put("department",department);
-        ArrayList<Department> depts = dao.find(hashMap);
+        ArrayList<Department> depts = daodept.find(hashMap);
         OutDepartment dept = (OutDepartment) depts.get(0);
         return dept.getNextWaitingPatient();
     }
