7179218289f242db71b347a4e5bceb8e683cdb13
simonmoesorensen <moe.simon@gmail.com>
2019-05-07 16:40:20 +0200

    Task: Observer pattern for departments
    Message:
    - Refactored tests in HospitalBedTest

--- src/test/java/tests/core/buildings/HospitalBedTest.java
+++ src/test/java/tests/core/buildings/HospitalBedTest.java
@@ -3,10 +3,12 @@ package tests.core.buildings;
 import core.buildings.Bed;
 import core.buildings.Department;
 import core.buildings.InDepartment;
+import core.buildings.OutDepartment;
 import core.persons.Clerk;
 import core.persons.Hospital;
 import core.persons.Patient;
 import core.persons.Staff;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,15 +20,26 @@ public class HospitalBedTest {
 
     private final Hospital hospital = new Hospital();
     private ArrayList<Department> depts = hospital.getDepartments();
-    private final Department department = new InDepartment("dep1", "ER", 10);
+    private Department department;
     private final Patient patient = new Patient("password123");
     private final Staff staff = new Clerk("password321");
-    private final Department department2 = new InDepartment("dep2", "ER2", 10);
+    private Department department2;
     private Bed bed;
 
     @Before
     public void setUp() {
+        department = new InDepartment("dep1", "ER", 10);
+        department2 = new InDepartment("dep2", "ER2", 10);
+
         bed = new Bed(10);
+        hospital.add(department);
+        hospital.add(department2);
+    }
+
+    @After
+    public void tearDown() {
+        hospital.remove(department);
+        hospital.remove(department2);
     }
 
     @Test
@@ -43,6 +56,7 @@ public class HospitalBedTest {
     public void fillOccupiedBed() {
         bed.fill (new Patient ());
         assertFalse (bed.fill (new Patient ()));
+        bed.empty();
     }
 
     @Test
@@ -60,6 +74,7 @@ public class HospitalBedTest {
     public void getPatientFilledBed() {
         bed.fill (new Patient ());
         assertNotNull (bed.getPatient ());
+        bed.empty();
     }
 
     @Test
@@ -71,93 +86,92 @@ public class HospitalBedTest {
 
     @Test
     public void assignPatient() {
-        hospital.add (department);
-        hospital.assign (patient, department);
+        assertTrue(hospital.assign(patient, department));
+        assertTrue(hospital.remove(patient, department));
     }
 
     @Test
     public void assignPatientDeptNonexistant() {
-        hospital.assign (patient, department);
-        depts = hospital.getDepartments();
-        assertTrue (depts.isEmpty ());
+        assertFalse(hospital.assign(patient, new OutDepartment("depTest123", "Saint Outz")));
+    }
+
+    @Test
+    public void addExistingDepartmentsFails() {
+        assertFalse(hospital.add(department));
+        assertFalse(hospital.add(department2));
     }
 
     @Test
     public void assignStaff() {
-        hospital.add (department);
-        hospital.assign (staff, department);
+        assertTrue(hospital.assign(staff, department));
+        assertTrue(hospital.remove(staff, department));
     }
 
     @Test
     public void assignStaffDeptNonexistant() {
-        hospital.assign (staff, department);
-        ArrayList<Department> depts = hospital.getDepartments();
-        assertTrue (depts.isEmpty ());
+        assertFalse(hospital.assign(staff, new OutDepartment("depTest123", "Saint Outz")));
     }
 
     @Test
     public void movePatient() {
+        assertTrue(hospital.assign(patient, department));
+        assertTrue(department.isPatientInDepartment(patient));
         hospital.move (patient, department, department2);
+        assertFalse(department.isPatientInDepartment(patient));
+        assertTrue(department2.isPatientInDepartment(patient));
     }
 
     @Test
     public void moveStaff() {
+        hospital.assign(staff, department);
+        assertTrue(department.isStaffInDepartment(staff));
         hospital.move (staff, department, department2);
+        assertFalse(department.isStaffInDepartment(staff));
+        assertTrue(department2.isStaffInDepartment(staff));
     }
 
     @Test
     public void removeDept() {
-        hospital.add(department);
-        hospital.remove(department);
-        assertTrue(depts.isEmpty());
+        assertFalse(depts.isEmpty());
+        assertTrue(hospital.remove(department));
     }
 
     @Test
     public void moveStaffTest() {
-        hospital.add(department);
-        hospital.add(department2);
-        department.add(staff);
+        assertTrue(hospital.assign(staff, department));
         hospital.move(staff, department, department2);
-        assertFalse(department.getStaff().contains(staff));
-        assertTrue(department2.getStaff().contains(staff));
+        assertFalse(department.isStaffInDepartment(staff));
+        assertTrue(department2.isStaffInDepartment(staff));
     }
 
     @Test
     public void movePatientTest() {
-        hospital.add(department);
-        hospital.add(department2);
-        department.add(patient);
-        hospital.move(patient, department, department2);
+        assertTrue(hospital.assign(patient, department));
+        assertTrue(hospital.move(patient, department, department2));
         assertFalse(department.getPatients().contains(patient));
         assertTrue(department2.getPatients().contains(patient));
     }
 
     @Test
     public void movePatientWhereItAlreadyExistsTest() {
-        hospital.add(department);
-        hospital.add(department2);
-        department.add(patient);
-        department2.add(patient);
-        hospital.move(patient, department, department2);
+        assertTrue(hospital.assign(patient, department));
+        assertTrue(hospital.assign(patient, department2));
+        assertFalse(hospital.move(patient, department, department2));
         assertTrue(department.getPatients().contains(patient));
         assertTrue(department2.getPatients().contains(patient));
     }
 
     @Test
     public void moveStaffWhereItAlreadyExistsTest() {
-        hospital.add(department);
-        hospital.add(department2);
-        department.add(staff);
-        department2.add(staff);
-        hospital.move(staff, department, department2);
+        assertTrue(hospital.assign(staff, department));
+        assertTrue(hospital.assign(staff, department2));
+        assertFalse(hospital.move(staff, department, department2));
         assertTrue(department.getStaff().contains(staff));
         assertTrue(department2.getStaff().contains(staff));
     }
 
     @Test
     public void getDepartmentsTest() {
-        hospital.add(department);
-        hospital.add(department2);
         assertTrue(hospital.getDepartments().contains(department));
         assertTrue(hospital.getDepartments().contains(department2));
     }
