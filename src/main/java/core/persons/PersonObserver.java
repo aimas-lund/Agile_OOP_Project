1aca7dc81e72b190880049989ebd67f3a611ebd1
AimasLund <aimas.lund@gmail.com>
2019-05-07 23:48:23 +0200

    Added docs to persons

--- src/main/java/core/persons/PersonObserver.java
+++ src/main/java/core/persons/PersonObserver.java
@@ -8,10 +8,27 @@ import persistence.data_access_objects.DaoStaffImpl;
 
 import javax.print.Doc;
 
+/**
+ * This class implements the Observer pattern, listening to any changes made
+ * to a Person class it is connected to.
+ *
+ * If changes are broadcasted to the PersonObserver, it immediately updates the
+ * database.
+ */
 public class PersonObserver implements Observer {
     DaoStaffImpl<Staff> daoStaff = new DaoStaffImpl<>();
     DaoPatientImpl<Patient> daoPatient = new DaoPatientImpl<>();
 
+    /**
+     * @param source
+     * @param action
+     * @param oldValue
+     * @param newValue
+     *
+     * Determines the type of the source which is changed. Then it calls itself again
+     * casting the source type.
+     *
+     */
     @Override
     public void objectChanged(Object source, Event action, Object oldValue, Object newValue) {
         if (source instanceof Staff) {
